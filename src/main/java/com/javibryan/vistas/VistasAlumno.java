8283df335d3b7b1e81a283dd5504efa25ec863bc
BatidoraCosmica <javier0728@gmail.com>
2020-05-14 16:23:44 +0200

    menuAlumnoActualizado

--- src/main/java/com/javibryan/vistas/VistasAlumno.java
+++ src/main/java/com/javibryan/vistas/VistasAlumno.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.javibryan.vistas;
 
-/**
- *
- * @author Usuario
- */
+import java.util.Scanner;
+
 public class VistasAlumno {
-    
+    public void menuAlumno() {
+        int opcion = 1;
+        while (opcion != 0) {
+            System.out.println("Introduzca la opción");
+            System.out.println("0.Salir");
+            System.out.println("1.Nuevo alumno");
+            System.out.println("2.Modificar alumno");
+            System.out.println("3.Asignar alumno a grupo");
+            System.out.println("4.Eliminar alumno");
+            System.out.println("5.Ver todos los alumnos");
+            
+            Scanner sc = new Scanner(System.in);
+            String entrada = sc.nextLine();
+            
+            if(!Utilidades.esEntero(entrada)) {
+                opcion = -1;
+            } else {
+                opcion = Integer.parseInt(entrada);
+            }
+            if (opcion == 1) {
+                this.nuevoAlumno();
+            }
+            if (opcion == 2) {
+                this.nuevoAlumno();
+            }
+            if (opcion = 3) {
+                this.nuevoAlumno();    
+            }
+            if (opcion = 4) {
+                this.nuevoAlumno();
+            }
+            if (opcion = 5) {
+                this.nuevoAlumno();
+            }
+        }
+    }
 }
