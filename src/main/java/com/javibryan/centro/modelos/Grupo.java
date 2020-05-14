72e2e37eff5d2cacdb7161611156ae16ef2fe199
BatidoraCosmica <javier0728@gmail.com>
2020-05-14 16:03:45 +0200

    TodoMezclado_Javi_Bryan

---
+++ src/main/java/com/javibryan/centro/modelos/Grupo.java
@@ -0,0 +1,5 @@
+package com.javibryan.centro.modelos;
+
+public class Grupo {
+    
+}
---
+++ src/main/java/com/javibryan/centros/Principal.java
@@ -0,0 +1,7 @@
+package com.javibryan.centros;
+
+public class Principal {
+    public static void main(String[] args) {
+        
+    }
+}
